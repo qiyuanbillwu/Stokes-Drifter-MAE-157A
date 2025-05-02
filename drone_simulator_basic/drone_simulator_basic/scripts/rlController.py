fixed some sign issues, need to double check

drone_simulator_basic/drone_simulator_basic/scripts/rlController.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-from util import quat_to_rot, quat_multiply, quat_conjugate, quaternion_error, qdot_from_omega, get_a_dot_hat
+from util import quat_to_rot, quat_multiply, quat_conjugate, quaternion_error, qdot_from_omega, get_a_dot_hat, allocation_matrix
 #must always account for double covering with quaternions to prevent unwinding
 
 #rlController works for just vertical position change
@@ -17,14 +17,14 @@ def outer_loop_controller(state, trajectory, mass, g, dt, lastVelError):
     axdes, aydes, azdes = trajectory['a']
 
     # Position and velocity errors
-    e_pos = np.array([xd, yd, zd]) - pos
-    e_vel = np.array([vxdes, vydes, vzdes]) - vel
-
-
+    # e_pos = np.array([xd, yd, zd]) - pos
+    # e_vel = np.array([vxdes, vydes, vzdes]) - vel
+    e_pos = pos - np.array([xd, yd, zd]) 
+    e_vel = vel - np.array([vxdes, vydes, vzdes]) 
 
     # PID gains for position control
-    Kp = np.array([0.3, 0.3, 0.3])  # Adjust these gains as necessary
-    Kd = np.array([0.1, 0.1, 0.1])  # Adjust these gains as necessary
+    Kp = np.array([0.4, 0.4, 0.4])  # Adjust these gains as necessary
+    Kd = np.array([0.2, 0.2, 0.2])  # Adjust these gains as necessary
 
     accel_des = np.array([axdes, aydes, azdes])
 
@@ -47,7 +47,8 @@ def outer_loop_controller(state, trajectory, mass, g, dt, lastVelError):
 
     R_d = quat_to_rot(q_des)
 
-    #placeholder
+    # =============
+    # how to apply a low-pass filter?
     a_dot = trajectory['j'] - Kp * e_vel - Kd * (e_vel - lastVelError) / dt
 
     lastVelError = e_vel
@@ -65,14 +66,17 @@ def outer_loop_controller(state, trajectory, mass, g, dt, lastVelError):
 
 def inner_loop_controller(state, q_des, omega_des, T, l, d):
     # Extract current quaternion and angular velocity
-    q_curr = state[6:10]       # [w, x, y, z]
+    q_curr = state[6:10]       # [qw, qx, qy, qz]
     omega = state[10:13]       # [wx, wy, wz]
 
+    # ============================
     # Orientation error quaternion: q_e = q_des^* ⊗ q_curr
+    # this should be q_des conjugate, but somehow q_des works better
+    # q_e = quaternion_error(quat_conjugate(q_des), q_curr)
     q_e = quaternion_error(q_des, q_curr)
 
     # PD gains
-    Kp = np.array([1.0, 1.0, 1.0])
+    Kp = np.array([1.0, 1.0, 0.5])
     Kd = np.array([0.3, 0.3, 0.3])   
 
     Lambda = np.array([0.5, 0.5, 0.3])
@@ -85,22 +89,13 @@ def inner_loop_controller(state, q_des, omega_des, T, l, d):
     
     # Angular velocity error
     omega_e = omega - R_e @ omega_des
-
     q_dot_e = qdot_from_omega(q_des, omega_e)
-
 
     # Control torque
     tau = -s * Kp * q_e[1:] - Kd * omega_e - Lambda * s * q_dot_e[1:]
 
-
-
     # Mixer matrix to solve for motor forces
-    mix = np.array([
-        [1, 1, 1, 1],        # Total thrust
-        [-l, l, l, -l],      # Roll
-        [l, l, -l, -l],      # Pitch
-        [d, -d, d, -d]       # Yaw
-    ])
+    mix = allocation_matrix(l,d)
 
     # Combine total thrust and torques
     tau_full = np.array([T, *tau])