corrected omega calculation and changed gain again

drone_simulator_basic/drone_simulator_basic/scripts/rlController.py
@@ -23,8 +23,8 @@ def outer_loop_controller(state, trajectory, mass, g, dt, lastVelError, prev_fil
     e_vel = vel - np.array([vxdes, vydes, vzdes]) 
 
     # PID gains for position control
-    Kp = np.array([0.4, 0.4, 0.4])  # Adjust these gains as necessary
-    Kd = np.array([0.2, 0.2, 0.2])  # Adjust these gains as necessary
+    Kp = np.array([1, 0.4, 10])  # Adjust these gains as necessary
+    Kd = np.array([10, 10, 10])  # Adjust these gains as necessary
 
     accel_des = np.array([axdes, aydes, azdes])
 
@@ -68,12 +68,12 @@ def outer_loop_controller(state, trajectory, mass, g, dt, lastVelError, prev_fil
 
     # omega is in the form of (wy, -wx, 0)
     # want omega_des in the form (wx, wy, 0)
-    #omega = R_d.T @ adot_hat
-    #omega_des = omega
-    #omega_des[0] = -omega[1]
-    #omega_des[1] = omega[0]
+    omega = R_d.T @ adot_hat
+    omega_des = omega
+    omega_des[0] = -omega[1]
+    omega_des[1] = omega[0]
 
-    omega_des = np.cross(a_hat, adot_hat)
+    # omega_des = np.cross(a_hat, adot_hat)
     omega_des[2] = 0  # if yaw is not tracked
 
     return T, q_des, omega_des, lastVelError, prev_filtered_derivative