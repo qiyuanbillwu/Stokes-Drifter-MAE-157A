03b6d4aff92242938a91105054077308ce1b41cb
Mr. Smiles <ryan@awkvi.com>
2025-06-03 23:06:28 -0700

    did prelim rl agent

--- drone_simulator_basic/drone_simulator_basic/scripts/drone_env.py
+++ drone_simulator_basic/drone_simulator_basic/scripts/drone_env.py
@@ -7,6 +7,8 @@ from trajectory import get_state
 from util import addNoiseToPercievedState
 from rlController import outer_loop_controller
 from rlController import inner_loop_controller
+from util import quat_to_rot, quat_multiply, quat_conjugate, qdot_from_omega, get_a_dot_hat, allocation_matrix
+
 
 lower_bound = 0.054*9.81
 upper_bound = 0.393*9.81
@@ -27,7 +29,7 @@ class DroneEnv(gym.Env):
         self.action_space = spaces.Box(low=0, high=1, shape=(4,), dtype=np.float32)
 
         # Observation:
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(20,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(26,), dtype=np.float32)
 
     def reset(self, seed=None, options=None):
         self.t = 0.0
@@ -44,7 +46,11 @@ class DroneEnv(gym.Env):
 
         pos_error = goal_pos - self.state[0:3]
         vel_error = goal_vel - self.state[3:6]
-        obs = np.concatenate([[self.t], self.state, pos_error, vel_error])
+
+        omega_error = [0,0,0]
+        q_e = [0,0,0]
+
+        obs = np.concatenate([[self.t], self.state, pos_error, vel_error, q_e, omega_error])
 
         return obs, {}
     
@@ -64,32 +70,44 @@ class DroneEnv(gym.Env):
         # Inner-loop controller gives the ground truth motor forces
         f_true = inner_loop_controller(self.state, q_des, omega_des, T, l, d)
 
-        # Simulate dynamics using agent's thrusts
-        self.state = self.dyn.propagate(self.state, f_agent, self.dt)
-        self.t += self.dt
-
         #print(f"f_agent: {f_agent}, f_true: {f_true}")
 
         goal_pos = desired['r']
         goal_vel = desired['v']
 
+        q_curr = self.state[6:10] 
+        omega = self.state[10:13] 
         pos_error = goal_pos - self.state[0:3]
         vel_error = goal_vel - self.state[3:6]
-        obs = np.concatenate([[self.t], self.state, pos_error, vel_error])
+        q_e = quat_multiply(quat_conjugate(q_des), q_curr)
+        R_e = quat_to_rot(q_e)
+
+        omega_error = omega - R_e @ omega_des
+        obs = np.concatenate([[self.t], self.state, pos_error, vel_error, q_e[1:], omega_error])
 
         # Reward is negative MSE between agent and true thrusts
         abs_error = np.mean(np.abs(f_agent - f_true))
         
-        reward = 1 - (1.0 * np.linalg.norm(pos_error) + 0.1 * np.linalg.norm(vel_error) + 0.5 * abs_error)
+        #reward = 10 - (0.05 * np.linalg.norm(pos_error) + 0.01 * np.linalg.norm(vel_error) + 0.01 * np.linalg.norm(q_e[1:]) + 0.01 * np.linalg.norm(omega_error) + 0.05 * abs_error)
+
+        reward = 1.0 - (5.0 * np.linalg.norm(pos_error) + 1.0 * np.linalg.norm(vel_error))
 
         terminated = self.state[2] < 0.1
         truncated = self.t >= self.max_time
 
+        reward *= 0.10
+
+        reward += 1.0
+
         if truncated and not terminated:
-            reward += 1000.0 
+            reward += 10.0 * (self.t / self.max_time)
 
         if terminated:
-            reward -= 1000.0
+            reward -= 100.0 * (1 - self.t / self.max_time)
+
+        # Simulate dynamics using agent's thrusts
+        self.state = self.dyn.propagate(self.state, f_agent, self.dt)
+        self.t += self.dt
 
         return obs, reward, terminated, truncated, {}
 
